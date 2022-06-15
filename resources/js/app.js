47d4f8deb74922b26c98ce09dacead923249e94d
Simarpreet Singh <simarpreet.singh@canvascraft.media>
2022-06-15 16:25:46 +0530

    Vue added into laravel application

---
+++ resources/js/app.js
@@ -0,0 +1,42 @@
+// Require Vue
+window.Vue = require('vue').default;
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+// Register Vue Components
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+import App from './App.vue';
+import Home from './components/HomeComponent'
+import Counter from './components/CounterComponent'
+import About from './components/AboutComponent'
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'HOME',
+        component: Home
+
+    },
+    {
+        path: '/counter',
+        name: 'Counter',
+        component: Counter
+    },
+    {
+        path:'/about',
+        name: 'About',
+        component: About
+    }
+
+]
+
+// Initialize Vue
+// const app = new Vue({
+//     el: '#app',
+// });
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
---
+++ resources/js/components/CounterComponent.vue
@@ -0,0 +1,28 @@
+<script>
+    export default{
+        data: () => {
+            return{
+                counter: 0
+            }
+        },
+        methods: {
+            incremnet () {
+                this.counter++
+            },
+            decrement () {
+                if(this.counter > 0){
+                    this.counter--
+                }
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <div>
+        <p>Count - {{counter}}</p>
+        <button v-on:click.prevent="incremnet" >Increment</button>
+        <button v-on:click.prevent="decrement" >Decrement</button>
+    </div>
+</template>
